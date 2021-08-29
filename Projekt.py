ca8ec5c9a04f56bd32ee140065013b680dc50be2
wakulak <wakulak@gmail.com>
2021-08-30 00:05:54 +0200

    first commit

---
+++ Projekt.py
@@ -0,0 +1,20 @@
+# Module Imports
+import mariadb
+import sys
+
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user="root@10.43.8.103",
+        password="K0m0rnik2013!",
+        host="10.43.8.103",
+        port=3306,
+        #database="wordpress"
+
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+cur = conn.cursor()
